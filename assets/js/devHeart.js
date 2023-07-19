a03a9aa8cf052e8b1cec3666fde69ec54b1759c3
benjammin4dayz <42326027+benjammin4dayz@users.noreply.github.com>
2023-07-19 06:48:24 -0400

    feat: hidden dev panel

--- assets/js/devHeart.js
+++ assets/js/devHeart.js
@@ -1,31 +1,11 @@
 const element = { isClicked: 0, handle: document.getElementById('devHeart') };
 element.handle.addEventListener('click', () => {
   !element.isClicked
-    ? (purgeCache(), applyDynamicStyles(element.handle))
+    ? (applyDynamicStyles(element.handle), revealDevButton())
     : location.reload();
   element.isClicked = !element.isClicked;
 });
 
-const purgeCache = async () => {
-  caches
-    .keys()
-    .then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          console.log(cacheName);
-          return caches.delete(cacheName);
-        })
-      );
-    })
-    .then(() => {
-      console.warn('Dev Heart: purged local caches...');
-    })
-    .catch((error) => {
-      console.error(
-        'An error occurred while attempting to purge local caches: ' + error
-      );
-    });
-};
 const applyDynamicStyles = (element, transitionDuration = 1000) => {
   ctx = element.style;
   ctx.color = 'red';
@@ -33,4 +13,12 @@ const applyDynamicStyles = (element, transitionDuration = 1000) => {
   ctx.transitionDuration = transitionDuration + 'ms';
   ctx.transformOrigin = 'center bottom';
   ctx.transform = 'translate(0, -3vh) scale(10)';
+};
+const revealDevButton = () => {
+  try {
+    document.getElementById('devPanel').style.visibility = 'visible';
+  } catch {}
+  try {
+    document.getElementById('devButton').style.display = 'inline-block';
+  } catch {}
 };
