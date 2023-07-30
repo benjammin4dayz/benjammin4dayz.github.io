fd70f63938103c96a4d5a66bb865493df2f86468
benjammin4dayz <42326027+benjammin4dayz@users.noreply.github.com>
2023-07-30 07:54:29 -0400

    fix: use cwd

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import terser from '@rollup/plugin-terser';
 export default {
   input: 'assets/js/_main.js',
   output: {
-    file: '_site/assets/js/bundle.js',
+    file: './_site/assets/js/bundle.js',
     format: 'iife',
     name: 'Bundle',
     sourcemap: false
